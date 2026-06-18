b2436495a8036fddb844bfd45ad5e516c1696abb
Grace Manning <gracebmanning@gmail.com>
2026-06-18 13:18:10 -0700

    bounce - UI changes, add nav

--- worlds/tommy-fleece-bounce/BounceEmbed.tsx
+++ worlds/tommy-fleece-bounce/BounceEmbed.tsx
@@ -3,11 +3,12 @@ export const BounceEmbed = () => {
         <iframe
             width="310"
             height="310"
-            src="https://www.youtube.com/embed/F6aYsopTmWo?si=Co8X6KguqXqtTQE5&amp;start=63&amp;autoplay=1&amp;controls=0&amp;loop=1"
+            src="https://www.youtube.com/embed/F6aYsopTmWo?si=Co8X6KguqXqtTQE5&amp;start=17&amp;autoplay=1&amp;controls=0&amp;loop=1"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerPolicy="strict-origin-when-cross-origin"
+            className="z-10 rounded-lg ring-2 ring-[#FF9700]"
         ></iframe>
     );
 };
--- worlds/tommy-fleece-bounce/Tommy-Fleece-Bounce.tsx
+++ worlds/tommy-fleece-bounce/Tommy-Fleece-Bounce.tsx
@@ -3,6 +3,7 @@ import WorldGate, { useWorldEntered } from "@/components/WorldGate";
 import { Site } from "@/lib/siteData";
 import dynamic from "next/dynamic";
 import { BounceEmbed } from "./BounceEmbed";
+import Link from "next/link";
 
 const BounceCanvas = dynamic(() => import("../tommy-fleece-bounce/BounceCanvas"), { ssr: false });
 const EMBED_SIZE = { width: 314, height: 314 };
@@ -13,7 +14,17 @@ function BounceContent() {
 
     return (
         <main className="relative w-full min-h-screen flex flex-col justify-center items-center bg-white">
-            <div className="relative z-10 border-2 border-pink-400">
+            <div className="relative z-10 w-full px-6 py-1 bg-neutral-100 flex flex-row justify-between items-center">
+                <Link href="/" className="text-[#0020BE] underline">
+                    ← go home
+                </Link>
+                <p className="w-fit text-sm text-[#127400] uppercase">Click in the white space</p>
+                <button className="text-[#C61200] underline hover:cursor-pointer">
+                    reset circle count
+                </button>
+            </div>
+
+            <div className="relative grow flex flex-col justify-center items-center">
                 <BounceEmbed />
             </div>
             <div className="absolute inset-0 z-0">
