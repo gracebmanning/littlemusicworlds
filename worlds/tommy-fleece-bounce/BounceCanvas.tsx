2bef9403a9c7cd35dd12644e6e0122830bc5ffac
Grace Manning <gracebmanning@gmail.com>
2026-06-18 12:22:40 -0700

    add bounce physics

--- worlds/tommy-fleece-bounce/BounceCanvas.tsx
+++ worlds/tommy-fleece-bounce/BounceCanvas.tsx
@@ -1,35 +1,90 @@
 "use client";
 import { useEffect, useRef } from "react";
 import paper from "paper";
+import Matter from "matter-js";
+
+const MIN_RADIUS = 25;
+const MAX_RADIUS = 65;
+const COLORS = ["#0020BE", "#C61200", "#127400", "#5F0F80", "#FF9700", "#E4539C"];
 
 export default function BounceCanvas() {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-    const MIN_RADIUS = 25;
-    const MAX_RADIUS = 65;
-
     useEffect(() => {
+        if (!canvasRef.current) return;
+        paper.setup(canvasRef.current); // initialize paper.js with the canvas
+
+        const canvasWidth = paper.view.viewSize.width;
+        const canvasHeight = paper.view.viewSize.height;
+
+        const { Engine, Bodies, Composite } = Matter;
+        const engine = Engine.create();
+        const world = engine.world;
+        engine.gravity.y = 1;
+
+        const thickness = 110;
+        Composite.add(world, [
+            Bodies.rectangle(
+                canvasWidth / 2,
+                canvasHeight + thickness / 2,
+                canvasWidth,
+                thickness,
+                { isStatic: true },
+            ),
+            Bodies.rectangle(-thickness / 2, canvasHeight / 2, thickness, canvasHeight * 2, {
+                isStatic: true,
+            }),
+            Bodies.rectangle(
+                canvasWidth + thickness / 2,
+                canvasHeight / 2,
+                thickness,
+                canvasHeight * 2,
+                { isStatic: true },
+            ),
+        ]);
+
+        const circles: { paperShape: paper.Path.Circle; matterBody: Matter.Body }[] = [];
+
+        const tool = new paper.Tool();
+
         let count = 0;
-        const colors = ["#0020BE", "#C61200", "#127400", "#5F0F80", "#FF9700", "#E4539C"];
+        tool.onMouseDown = function (event: paper.ToolEvent) {
+            const x = event.point.x;
+            const y = event.point.y;
+            const radius = Math.floor(Math.random() * (MAX_RADIUS - MIN_RADIUS + 1)) + MIN_RADIUS;
 
-        if (canvasRef.current) {
-            paper.setup(canvasRef.current); // initialize paper.js with the canvas
+            const matterCircle = Bodies.circle(x, y, radius, {
+                restitution: 1,
+            });
+            Composite.add(world, matterCircle);
 
-            const tool = new paper.Tool();
+            const paperCircle = new paper.Path.Circle({
+                center: event.point,
+                radius: radius,
+                fillColor: COLORS[count],
+            });
 
-            tool.onMouseDown = function (event: paper.ToolEvent) {
-                const circle = new paper.Path.Circle({
-                    center: event.point,
-                    radius: Math.floor(Math.random() * (MAX_RADIUS - MIN_RADIUS + 1)) + MIN_RADIUS,
-                    fillColor: colors[count],
-                });
-                count = count === colors.length - 1 ? 0 : count + 1;
-            };
-        }
+            circles.push({
+                paperShape: paperCircle,
+                matterBody: matterCircle,
+            });
+
+            count = count === COLORS.length - 1 ? 0 : count + 1;
+        };
+
+        paper.view.onFrame = function (event: paper.Event) {
+            Engine.update(engine);
+
+            for (const { paperShape, matterBody } of circles) {
+                paperShape.position = new paper.Point(matterBody.position.x, matterBody.position.y);
+            }
+        };
 
         return () => {
+            tool.remove();
             paper.project?.remove();
             paper.view?.remove();
+            Engine.clear(engine);
         };
     }, []);
 
