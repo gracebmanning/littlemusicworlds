fb0f610e8c71c45ff96cdfc97ef4880e132d0ee0
Grace Manning <gracebmanning@gmail.com>
2026-07-12 21:32:35 -0700

    add song embed, reset button, back button

--- worlds/loukeman-pink-bape-lighter/Loukeman-Pink-Bape-Lighter.tsx
+++ worlds/loukeman-pink-bape-lighter/Loukeman-Pink-Bape-Lighter.tsx
@@ -1,12 +1,33 @@
 "use client";
+import Link from "next/link";
 import WorldGate from "@/components/WorldGate";
 import { Site } from "@/lib/siteData";
 import RainbowScratchPaper from "./RainbowScratchPaper";
 
+const SongEmbed = () => {
+    return (
+        <div className="fixed bottom-2 right-2 z-20 w-30 aspect-square overflow-hidden">
+            <iframe
+                className="absolute top-0 left-1/2 -translate-x-1/2 h-full w-[178%]"
+                src="https://www.youtube.com/embed/ydGyN1fMuR8?si=RZxLi7B8d_Wzip0D&amp;autoplay=1&amp;controls=0"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+            ></iframe>
+        </div>
+    );
+};
+
 function PinkBapeLighterContent() {
     return (
-        <main className="w-full h-dvh flex flex-col justify-center items-center bg-black">
+        <main className="relative w-full h-dvh flex flex-col justify-center items-center bg-black">
+            <Link href="/" className="absolute top-2 left-2 z-20 w-fit underline">
+                ← back
+            </Link>
             <RainbowScratchPaper />
+            <SongEmbed />
         </main>
     );
 }
